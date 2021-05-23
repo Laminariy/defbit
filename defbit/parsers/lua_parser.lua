ee603cf10f1e73dff380d40a72f480ffc3be3610
Laminariy <dovakin121212@gmail.com>
2021-05-23 21:19:13 +0300

    lua_parser and tests

--- defbit/parsers/lua_parser.lua
+++ defbit/parsers/lua_parser.lua
@@ -1,11 +1,25 @@
+require "defbit.parsers.data_dumper"
+
+
 local M = {}
 
 function M.encode(type, data)
-
+	local data = {
+		type = type,
+		data = data
+	}
+	data = DataDumper(data, _, true)
+	return data
 end
 
 function M.decode(data)
-
+	-- могут ли выбивать ошибки?
+	-- assert, pcall
+	local decoded = loadstring(data)
+	local env = {}
+	setfenv(decoded, env)
+	decoded = decoded()
+	return decoded
 end
 
 return M
