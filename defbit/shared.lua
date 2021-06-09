be61dda4a64cb3efdac4c80a0b9ad55d3e3c1191
Laminariy <dovakin121212@gmail.com>
2021-06-09 18:36:11 +0300

    shared refactoring

--- defbit/shared.lua
+++ defbit/shared.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+local function deepcopy(orig)
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in next, orig, nil do
+			copy[deepcopy(orig_key)] = deepcopy(orig_value)
+		end
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
+
+
 function M.new_shared(connection, parser)
 	local shared = {
 		connection = connection,
@@ -14,179 +29,224 @@ function M.new_shared(connection, parser)
 	}
 
 	function shared._get_new_table(self, shared_table)
-		local id, del_access, fields = shared_table.id, shared_table.del_access, shared_table.fields
-
-		local tbl = {}
-		local meta = {
-			id = id,
-			_shared = true,
-			del_access = del_access,
-			fields = {} --{field_name = {value, rights, was_changed}}
-		}
-
-		for key, value in pairs(fields) do
-			meta.fields[key] = {value = value.value, rights = value.rights, was_changed = false}
-		end
-
-		function meta.__newindex(tbl, key, value)
-			if meta.fields[key] then
-				if self.type == meta.fields[key].rights or meta.fields[key].rights == 'both' then
-					meta.fields[key].was_changed = true
-					meta.fields[key].value = value
-				end
-			else
-				rawset(tbl, key, value)
-			end
-		end
-
-		function meta.__index(tbl, key)
-			if meta.fields[key] then
-				return meta.fields[key].value
-			else
-				return rawget(tbl, key)
-			end
-		end
-
-		setmetatable(tbl, meta)
-		table.insert(self.shared_tables, tbl)
+		local id, fields, options = shared_table.id, shared_table.fields, shared_table.options
+		local shared_table = self.create(fields)
+		local meta = getmetatable(shared_table)
+		meta.set_id(id)
+		meta.set_options(options)
+		table.insert(self.shared_tables, shared_table)
 
 		if self.add_listener then
-			self.add_listener(tbl)
+			self.add_listener(shared_table)
 		end
 	end
 
 	function shared._get_removed_table(self, shared_table)
 		local id = shared_table.id
-
-		for i, tbl in ipairs(self.shared_tables) do
-			if getmetatable(tbl).id == id then
+		for i, sh_tbl in ipairs(self.shared_tables) do
+			if getmetatable(sh_tbl).id == id then
 				table.remove(self.shared_tables, i)
+				local meta = getmetatable(sh_tbl)
+				meta.set_id(_)
+				meta.set_options(_)
+				meta.set_update_listener(_)
 				if self.remove_listener then
-					self.remove_listener(tbl)
+					self.remove_listener(sh_tbl)
 				end
 				break
-			end
-		end
-	end
-
-	local function unparse_sh_fields(self, tbl, meta, fields)
-		-- TO DO: unparse
-		local field_names = {}
-		for key, value in pairs(fields) do
-			if meta.fields[key].rights == 'both' or self.type ~= meta.fields[key].rights then
-				if type(value) == 'table' then
-					
-				else
-					rawset(tbl, key, value)
-				end
-				table.insert(field_names, key)
 			end
 		end
 	end
 
 	function shared._get_sync(self, sync_table)
-		local id = sync_table.id
-		for _, tbl in ipairs(self.shared_tables) do
-			local meta = getmetatable(tbl)
+		local id, fields = sync_table.id, sync_table.fields
+		for _, sh_tbl in ipairs(self.shared_tables) do
+			local meta = getmetatable(sh_tbl)
 			if meta.id == id then
-				local field_names = {}
-				for key, value in pairs(sync_table.fields) do
-					if meta.fields[key].rights == 'both' or self.type ~= meta.fields[key].rights then
-						if type(value) == 'table' then
-							
-						else
-							rawset(tbl, key, value)
-						end
-						table.insert(field_names, key)
-					end
-				end
-				if meta.on_update then
-					meta.on_update(tbl, field_names)
-				end
+				meta.set_changed_data(fields)
 				break
 			end
 		end
 	end
 
 
-	function shared.create(self, tbl, options)
-		-- TO DO: one shared table in many clients
-		-- was_changed on defbit.client, not in shared_table
-		--[[
-		options = {
-			on_update(table, fields),
-			rights = {field_name = "both", "server", "client"},
-			del_access = "server", "client", "both"
-		}
-		]]
-
+	function shared.create(tbl)
 		local meta = {
-			_shared = true,
-			del_access = options.del_access or "both",
-			fields = {}, --{field_name = {value, rights, was_changed}}
-			on_update = options.on_update
-		}
+			id = _,
+			options = _,
+			on_update = _,
 
-		for field, rights in pairs(options.rights) do
-			meta.fields[field] = {value = tbl[field], rights = rights, was_changed = false}
-			tbl[field] = nil
-		end
+			__is_shared = true,
+			__shared_fields = deepcopy(tbl),
+			__changed_fields = {} -- {key: is_changed}
+		}
 
 		function meta.__newindex(tbl, key, value)
-			if meta.fields[key] then
-				if self.type == meta.fields[key].rights or meta.fields[key].rights == 'both' then
-					meta.fields[key].was_changed = true
-					meta.fields[key].value = value
+			if meta.options then
+				local type, rights = meta.options.type, meta.options.rights
+				if rights[key] and type ~= rights[key] and rights[key] ~= 'both' then
+					return
 				end
-			else
-				rawset(tbl, key, value)
 			end
+			meta.__shared_fields[key] = value
+			meta.__changed_fields[key] = true
 		end
 
 		function meta.__index(tbl, key)
-			if meta.fields[key] then
-				return meta.fields[key].value
-			else
-				return rawget(tbl, key)
+			return meta.__shared_fields[key]
+		end
+
+		function meta.clear_changed_fields()
+			for key, _ in pairs(meta.__changed_fields) do
+				meta.__changed_fields[key] = nil
 			end
 		end
 
-		setmetatable(tbl, meta)
-
-		return tbl
-	end
-
-	function shared.add(self, shared_table)
-		local meta = getmetatable(shared_table)
-		meta.id = self.type..'_'..self.shared_ids
-		self.shared_ids = self.shared_ids + 1
-		local sh_table = {
-			id = meta.id,
-			del_access = meta.del_access,
-			fields = {}
-		}
-
-		for key, value in ipairs(meta.fields) do
-			sh_table.fields[key] = {value = value.value, rights = value.rights}
+		function meta.set_id(id)
+			if meta.id and id then
+				error('table already shared', 3)
+				return
+			end
+			meta.id = id
 		end
 
-		local data = self.parser.encode('shared_add', sh_table)
-		self.connection:send(data)
+		function meta.set_options(options)
+			--[[
+			options = {
+				type = 'client', 'server'
+				on_update(table, fields),
+				rights = {field_name = "both", "server", "client"},
+				del_access = "server", "client", "both"
+			}
+			]]
+			if options then
+				meta.on_update = options.on_update
+				options.on_update = nil
+			end
+			meta.options = options
+		end
 
-		table.insert(self.shared_tables, shared_table)
-	end
+		function meta.set_update_listener(listener)
+			meta.on_update = listener
+		end
 
-	function shared.remove(self, shared_table)
-		local meta = getmetatable(shared_table)
-		if meta.del_access == self.type or meta.del_access == 'both' then
-			for i, tbl in ipairs(self.shared_tables) do
-				if tbl == shared_table then
-					local data = self.parser.encode('shared_remove', {id = meta.id})
-					self.connection:send(data)
-					table.remove(self.shared_tables, i)
-					break
+		function meta.get_changed_data()
+			local changed = {}
+			changed.__is_shared = true
+			for key, _ in pairs(meta.__changed_fields) do
+				if getmetatable(meta.__shared_fields[key]).__is_shared then
+					changed[key] = getmetatable(meta.__shared_fields[key]).get_changed_data()
+				else
+					changed[key] = meta.__shared_fields[key]
 				end
 			end
+			return changed
+		end
+
+		function meta.get_full_data()
+			local data = {}
+			data.__is_shared = true
+			for key, value in pairs(meta.__shared_fields) do
+				if getmetatable(value).__is_shared then
+					data[key] = getmetatable(value).get_full_data()
+				else
+					data[key] = value
+				end
+			end
+			return data
+		end
+
+		function meta.set_changed_data(changed_data)
+			changed_data.__is_shared = nil
+			local changed_fields = {}
+			local type, rights = meta.options.type, meta.options.rights
+			for key, value in pairs(changed_data) do
+				if type ~= rights[key] or rights[key] == 'both' then
+					table.insert(changed_fields, key)
+					if value.__is_shared then
+						getmetatable(meta.__shared_fields[key]).set_changed_data(value)
+					else
+						meta.__shared_fields[key] = value
+					end
+				end
+			end
+			if meta.on_update then
+				meta.on_update(meta.__child, changed_fields)
+			end
+		end
+
+		meta.__child = setmetatable({}, meta)
+		return meta.__child
+	end
+
+	function shared.add(self, shared_tbl, options)
+		local meta = getmetatable(shared_tbl)
+		assert(meta.__is_shared, 'you must provide shared table')
+
+		meta.set_id(self.type..'_'..self.shared_ids)
+		self.shared_ids = self.shared_ids + 1
+
+		options.type = self.type
+		meta.set_options(options)
+
+		table.insert(self.shared_tables, shared_tbl)
+
+		local data = {
+			id = meta.id,
+			fields = meta.__shared_fields,
+			options = options
+		}
+		data = self.parser.encode('shared_add', data)
+		self.connection:send(data)
+	end
+
+	function shared.remove(self, shared_tbl)
+		local meta = getmetatable(shared_tbl)
+		assert(meta.__is_shared, 'you must provide shared table')
+		assert(meta.id, 'table must be added to client')
+
+		local del_access = meta.options.del_access
+		if del_access ~= self.type and del_access ~= 'both' then
+			return
+		end
+
+		for i, sh_tbl in ipairs(self.shared_tables) do
+			if shared_tbl == sh_tbl then
+				local data = self.parser.encode('shared_remove', {id = meta.id})
+				self.connection:send(data)
+				table.remove(self.shared_tables, i)
+				break
+			end
+		end
+
+		meta.set_id(_)
+		meta.set_options(_)
+		meta.set_update_listener(_)
+	end
+
+	function shared.delta_sync(self)
+		for _, shared_table in ipairs(self.shared_tables) do
+			local meta = getmetatable(shared_table)
+			local sync_tbl = {
+				id = meta.id,
+				fields = meta.get_changed_data()
+			}
+			local data = self.parser.encode('shared_sync', sync_tbl)
+			self.connection:send(data)
+			meta.clear_changed_fields()
+		end
+	end
+
+	function shared.full_sync(self)
+		for _, shared_table in ipairs(self.shared_tables) do
+			local meta = getmetatable(shared_table)
+			local sync_tbl = {
+				id = meta.id,
+				fields = meta.get_full_data()
+			}
+			local data = self.parser.encode('shared_sync', sync_tbl)
+			self.connection:send(data)
+			meta.clear_changed_fields()
 		end
 	end
 
@@ -198,76 +258,6 @@ function M.new_shared(connection, parser)
 	function shared.set_remove_listener(self, listener)
 		-- listener(shared_table)
 		self.remove_listener = listener
-	end
-
-	function shared.set_update_listener(self, shared_table, listener)
-		local meta = getmetatable(shared_table)
-		meta.on_update = listener
-	end
-
-	local function parse_d_sh_fields(fields)
-		local sh_fields = {}
-
-		for field, data in pairs(fields) do
-			if data.was_changed then
-				sh_fields[field] = data.value
-				data.was_changed = false
-			end
-			if getmetatable(data.value)._shared then
-				local fields = getmetatable(data.value).fields
-				sh_fields[field] = parse_d_sh_fields(fields)
-			end
-		end
-
-		return sh_fields
-	end
-
-	function shared.delta_sync(self)
-		for _, tbl in ipairs(self.shared_tables) do
-			local meta = getmetatable(tbl)
-			local sync_tbl = {
-				id = meta.id,
-				fields = parse_d_sh_fields(meta.fields)
-			}
-
-			if next(sync_tbl.fields) then
-				local data = self.parser.encode('shared_sync', sync_tbl)
-				self.connection:send(data)
-			end
-		end
-	end
-
-	local function parse_f_sh_fields(self, fields)
-		local sh_fields = {}
-
-		for field, data in pairs(fields) do
-			if data.rights == self.type or data.rights == 'both' then
-				if getmetatable(data.value)._shared then
-					local fields = getmetatable(data.value).fields
-					sh_fields[field] = parse_f_sh_fields(self, fields)
-				else
-					sh_fields[field] = data.value
-				end
-			end
-			data.was_changed = false
-		end
-
-		return sh_fields
-	end
-
-	function shared.full_sync(self)
-		for _, tbl in ipairs(self.shared_tables) do
-			local meta = getmetatable(tbl)
-			local sync_tbl = {
-				id = meta.id,
-				fields = parse_f_sh_fields(self, meta.fields)
-			}
-
-			if next(sync_tbl.fields) then
-				local data = self.parser.encode('shared_sync', sync_tbl)
-				self.connection:send(data)
-			end
-		end
 	end
 
 	return shared
